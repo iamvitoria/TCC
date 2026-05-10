e22e0ad158f0335b12a26809030cfc9b28b0a962
Vitória Camara <vlcamara@inf.ufsm.br>
2026-05-10 15:25:07 -0300

    Update Register.jsx

--- ecomonitor/src/pages/Register.jsx
+++ ecomonitor/src/pages/Register.jsx
@@ -38,7 +38,7 @@ const Register = () => {
           nome: nome,
           email: email,
           senha: senha,
-          perfil: "user"
+          cidade: "Não informada"
         }),
       });
 
