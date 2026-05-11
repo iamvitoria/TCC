f522217b2311257d7513718678f2c75d6e38179d
Vitória Camara <vlcamara@inf.ufsm.br>
2026-05-11 09:55:22 -0300

    Update App.jsx

--- ecomonitor/src/App.jsx
+++ ecomonitor/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import API_URL from "./config"; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login"; 
 import Register from "./pages/Register";
@@ -15,6 +16,13 @@ import AdminPerfil from './pages/AdminPerfil';
 import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    fetch(`${API_URL}/`)
+      .then(() => console.log("Servidor aquecido"))
+      .catch(() => console.log("Servidor ainda acordando"));
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
